cbe1035a90f84588d924800025704c1a5ca2c616
MobuchiElly <ugwueleazer@gmail.com>
2025-01-02 10:36:05 +0100

    feat: implemented google analytics to track user activity and impression

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,25 @@ import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import store from "@/redux/store";
 import { Provider } from "react-redux";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
+    <>
+      <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ENDPOINT}`}></Script>
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ENDPOINT}');
+        `}
+      </Script>
+      <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+    </>
   );
 }
